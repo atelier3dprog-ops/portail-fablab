ed84b3872c6cc21ec3668189135c375fea93e61e
Patge44 <pguerin.allehose@gmail.com>
2026-03-12 12:19:26 +0100

    Correction des chemins pour l'organisation

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: 'https://github.com/atelier3dprog/Portail_Doc_Atelier3dprog.git', // (N'oubliez pas de mettre le nom de votre dépôt GitHub ici pour l'étape 4)
-  
+  // On indique à Vite que le site est dans un sous-dossier
+  base: '/Portail_Doc_Atelier3dprog/',
 })
