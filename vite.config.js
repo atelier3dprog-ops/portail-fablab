11639f58316c9384c5f21993577515ed94c59a0b
Patge44 <pguerin.allehose@gmail.com>
2026-03-12 11:41:00 +0100

    nouveau build pour prise en compte des corrections d'erreurs

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: 'https://github.com/atelier3dprog-ops/portail-fablab.git', // (N'oubliez pas de mettre le nom de votre dépôt GitHub ici pour l'étape 4)
+  base: 'https://github.com/atelier3dprog/Portail_Doc_Atelier3dprog.git', // (N'oubliez pas de mettre le nom de votre dépôt GitHub ici pour l'étape 4)
+  
 })
